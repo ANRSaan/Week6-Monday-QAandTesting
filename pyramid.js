630cf157a7509d9d7ceefb3531cabaf744a3780c
Brian Williams <heffergod@gmail.com>
2022-09-19 09:30:32 -0700

    aded Bek's method for the tower problem, which I think is better

--- pyramid.js
+++ pyramid.js
@@ -26,3 +26,18 @@ const pyramid = num => {
 
 console.log(pyramid(3))
 
+const towerOf = num => {
+    let tower = []              // set tower equal to an empty array
+    let stars = 1              // set 'stars' equal to 1 -> the top of each tower will have 1 star
+    let row = ''              // set row equal to an empty string
+  
+    for (let i = num - 1; i >= 0; i--) {              // reverse for loop (i will track number of spaces before an after the stars on each row)
+      row = ' '.repeat(i) + '*'.repeat(stars) + ' '.repeat(i)      // use .repeat function to add the appropriate number of spaces and * per row
+      tower.push(row)              // push the current string to the array
+      row = ''                    // reset the row string to be empty
+      stars += 2                  // add 2 to the star variable (since 1 space is taken off of each end of the row, two stars need to be added)
+    }
+    
+    return tower
+  }
+
